fa90e76ceb0d9ea1f36534bc5c0db10815f47996
morykanm <morykanm@moravian.edu>
2020-04-25 10:01:13 -0400

    Created the route for an individual and calculated fitness

--- geneticAlgorithm.py
+++ geneticAlgorithm.py
@@ -2,7 +2,7 @@ import random
 import numpy
 
 
-class city:
+class City:
     """
     Creates the city as a pair of x and y coordiantes.
     Can calculate the distance between two cities
@@ -18,6 +18,47 @@ class city:
         return numpy.sqrt(x_distance**2 + y_distance**2)
 
         
+class Individual:
+    """
+    Creates an individual by creating its own gene sequence
+    Calculates the fitness quality of an individual
+    """
+    def __init__(self, gene_pool):
+        self.gene_pool = gene_pool
+        
+
+    def create_gene_sequence(self):
+        """
+        Creates one individual by randomly taking a sample from the gene pool and adding the home city on the end
+        """
+        gene_sequence = random.sample(list(self.gene_pool), len(self.gene_pool))
+        gene_sequence += gene_sequence[0]
+        return gene_sequence
+
+
+    def determine_fitness(self):
+        total_distance = 0
+        gene_sequence = self.get_gene_sequence()
+        for gene in range(len(gene_sequence)):
+            if gene == len(gene_sequence) - 1:
+                break
+            city_1 = self.gene_pool[gene_sequence[gene]]
+            print(city_1.x_coord, city_1.y_coord)
+            city_2 = self.gene_pool[gene_sequence[gene + 1]]
+            print(city_2.x_coord, city_2.y_coord)
+            total_distance += city_1.get_distance(city_2)
+
+        return total_distance
+
+
+    def get_gene_sequence(self):
+        return self.create_gene_sequence()
+
+
+    def get_fitness(self):
+        return self.determine_fitness()
+    
+
 def create_gene_pool():
     """
     Create a fixed amount of genes that one animal can have
@@ -28,24 +69,20 @@ def create_gene_pool():
             ']', '{', '}', '|', ';', '<', '>', '/', '?']
     gene_pool = {}
     for gene in genes:
-        gene_pool[gene] = city(random.randint(0, 10000), random.randint(0, 10000))
+        gene_pool[gene] = City(random.randint(0, 10000), random.randint(0, 10000))
 
-    return gene_pool
-
-
-def create_gene_sequence(gene_pool):
-    """
-    Creates one individual by randomly taking a sample from the gene pool and adding the home city on the end
-    """
-    gene_sequence = random.sample(list(gene_pool), len(gene_pool))
-    gene_sequence += gene_sequence[0]
-    return gene_sequence
+    return gene_pool    
 
 
 def main():
     gene_pool = create_gene_pool()
-    gene_sequence = create_gene_sequence(gene_pool)
-    print(gene_sequence)
+    first_individual = Individual(gene_pool)
+    sequence = first_individual.create_gene_sequence()
+    fitness = first_individual.get_fitness()
+    print(sequence)
+    print(fitness)
+
+
 
 if __name__ == '__main__':
     main()
